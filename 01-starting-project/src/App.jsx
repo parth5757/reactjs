bdbcaaf8f359c05912386df167df830925af4d2c
parth5757 <psthakkar2@gmail.com>
2024-05-10 16:51:46 +0530

    react useState added

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -2,10 +2,26 @@ import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButoon.jsx';
+import { useState } from 'react';
 
 function App() {
+  // Always with an state the it declare with the array structure value where first selectedTopic is showing number of count time & set selected is an function
+  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  // we simply think to change the component state we think we can directly use this but this thing is not work because without using state we can't change the value of an component in page without  
+  // let tabContent = 'Please click a button'
+
+  function handleSelect(selectedButton){
+    // Selected Button => 'components', 'jsx', 'props', 'state'
+    // tabContent = selectedButton;
+    // now using usestate we can change value of of selected topic 
+    // console.log(tabContent);
+    setSelectedTopic(selectedButton)
+    console.log(selectedTopic)
+  }
+
+  console.log('APP COMPONENT EXECUTING')
   return (
-    <div>
+    <div> 
       <Header />
       <main>
         <section id="core-concepts">
@@ -37,12 +53,15 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>Jsx</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('Jsx')}>Jsx</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
             {/* <TabButton label='Components'></TabButton>*/} {/* another component use method */}
           </menu>
+          {/* so now we will easily replace this tabContent from here because  it will not work we have to use useState function */}
+          {/* {tabContent} */}
+          {selectedTopic}
         </section>
       </main>
     </div>
--- 01-starting-project/src/components/TabButoon.jsx
+++ 01-starting-project/src/components/TabButoon.jsx
@@ -6,19 +6,20 @@
 //     )
 // } //one methods with use label
 
-// export default function TabButton({ children } ) {
-//     return (
-//         <li>
-//             <button>{children}</button>
+
+// export default function TabButton(props) {
+    //     return (
+        //         <li>
+        //             <button>{props.children}</button>
 //         </li>
 //     )
 // } //one methods with use with just props
 
-
-export default function TabButton(props) {
+export default function TabButton({ children, onSelect } ) {
+    console.log('TABBUTTON COMPONENT EXECUTING')
     return (
         <li>
-            <button>{props.children}</button>
+            <button onClick={onSelect}>{children}</button>
         </li>
     )
-}
+} 
