a23e79b5e7d2218d9bba2e7ccda1c385a3bcde1e
parth5757 <psthakkar2@gmail.com>
2024-05-13 11:25:08 +0530

    conversion of data type

---
+++ JavaScript/01basics/03_conversionOperator.js
@@ -0,0 +1,35 @@
+let score = "33ab"
+
+console.log(typeof score);
+
+console.log(typeof (score));
+
+let valueNumber = Number(score);
+
+console.log(typeof valueNumber);
+
+console.log(valueNumber);
+
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1
+// undefined => NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringToNumber =    String(someNumber)
+console.log(stringToNumber)
+
+
+
+
+
