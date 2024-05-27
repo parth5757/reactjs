9a129e5bbbcebc8597a2aad68c775651607a876c
parth5757 <psthakkar2@gmail.com>
2024-05-27 07:49:48 +0530

    default src removed and new src added

--- block_word_extension/background.js
+++ block_word_extension/background.js
@@ -1,8 +1,10 @@
 chrome.runtime.onInstalled.addListener(() => {
     // Initialize the list of keywords
-    chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "korean","study glows", "ankit awasthi", "status", "reel", "shorts", "#fun", "Taarak Mehta Ka Ooltah Chashmah", "News"] });
+    chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "korean","study glows", "ankit awasthi", "status", "reel", "shorts", "fun", "Taarak Mehta Ka Ooltah Chashmah", "affairs", "drama"] });
   });
+
   
+  // this is old one it work when the tab reloaded 
   chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status === 'complete' && tab.title) {
       chrome.storage.local.get("keywords", (data) => {
@@ -16,3 +18,5 @@ chrome.runtime.onInstalled.addListener(() => {
       });
     }
   });
+
+// this is new version where that tab is always been checking 
