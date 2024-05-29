5594e1d68f0760eea4e05ebb035f055956b8afad
parth5757 <psthakkar2@gmail.com>
2024-05-29 22:19:15 +0530

    dashboard app update

---
+++ dashboard/Readme.md
@@ -0,0 +1,23 @@
+# React Admin Panel Dashboard Project
+
+this is an react admin panel project for common use
+
+to install this project you can use the following command
+
+- to install this project just follow the step
+
+```bash
+git pull https://github.com/parth5757/reactjs.git
+```
+
+```bash
+cd dashboard
+```
+
+```bash
+npm install
+```
+
+```bash
+npm start
+```
--- dashboard/src/App.js
+++ dashboard/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
 import './App.css'
 const App = () => {
+  const activeMenu =  true;
   return (
-    <h1 className='underline text-3xl'>App</h1>
-    
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: '1000'}}>
+            <TooltipComponent content="Settings" position="Top">
+              <button type="button" className="text-3xl p-3 hover:drop-shadow-xl text-white" style={{ background: 'blue', borderRadius:'50%'}}>
+                <FiSettings />
+              </button>
+            </TooltipComponent >
+          </div>
+          {activeMenu ?(
+            <div>
+              sidebar
+            </div>
+          ) : (
+            <div>
+              sidebar w-0
+            </div>
+          )
+          }
+        </div>
+      </BrowserRouter>
+    </div>
   )
 }
 
