f3ca59ed5ddcce2af38812e009703bdb4035acec
parth5757 <psthakkar2@gmail.com>
2024-07-07 15:05:56 +0530

    block_word_extension updated new word

--- block_word_extension/background.js
+++ block_word_extension/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "minitv", "mx player", "south movie", "korean","study glows", "ankit awasthi", "status", "reels", "shorts", "Sony Sab", "affairs", "drama", "food Vlog", "street food", "Jio Cinema", "Cricket", "Pahul Walia", "rj raunac", "tanay", "Dhruv Rathee", "swagger sharma", "web series", "movie"] });
+  chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "minitv", "mx player", "south movie", "korean","study glows", "ankit awasthi", "status", "reels", "shorts", "Sony Sab", "affairs", "drama", "food Vlog", "street food", "Jio Cinema", "Cricket", "Pahul Walia", "rj raunac", "tanay", "Dhruv Rathee", "swagger sharma", "web series", "movie", "studyiq", "yrkkh", "bollywood", "roast", "hindi knowledge show", "pakistan", "china", "Physics Wallah", "esral", "maddam sir", "russia", "song", "sonyliv", "study i" ,"tenali", "son", "virat kohli", "Rohit Sharma", "Rohit", "IPL", "RCB", "GT", "Darshan Raval", "Arjit singh", "T seies", "neha kakkar", "Ashish Chanchalni", "Triggered inssan", "Fukra inssan", "world affairs", "unacadmy", "csk", "t-series"] });
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
--- block_word_extension/content.js
+++ block_word_extension/content.js
@@ -1,6 +1,13 @@
 if (window.location.href.includes("https://www.linkedin.com/feed/")) {
   window.location.href = "https://www.linkedin.com/jobs/";
 }
+// else if (window.location.href.includes("https://www.udemy.com/course/react-the-complete-guide-incl-redux/")) {
+//   alert("executing")
+//   $(document).ready(function() {
+//     alert("entering in the word")
+//     $('.ud-text-xl video-viewer--title-overlay--YZQuH').html('');
+//   });
+// }
 
 function checkTitle() {
     const title = document.title;
