1755d3dfa8c70d38b9b7cc3e8d06b874614dddbe
parth5757 <psthakkar2@gmail.com>
2024-07-07 17:57:21 +0530

    javascript number file added

---
+++ JavaScript/01basics/08_nums_and_math.js
@@ -0,0 +1,45 @@
+const score = 400
+// console.log(100);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// // console.log(balance.toString().length);   //here we use one of the great thing of javascript that any data type of javascript we can directly use that method into our code here we can directly use the string method length
+
+// console.log(balance.toFixed(2));
+
+
+const anyNumber = 25.5654
+// console.log(anyNumber.toPrecision(2));
+
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// Number.
+// it is having an big number of method are available in the by default number 
+
+
+// +++++++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+
+// console.log(Math);
+
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.3));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+
+console.log(Math.random() * 10); // it give always value in between 0 and 1
+console.log(Math.random()*10 +1 );
+console.log((Math.random()*10) +1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
