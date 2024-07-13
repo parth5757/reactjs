cc3ab57104e6686778284d7922d7e9913f125126
parth5757 <psthakkar2@gmail.com>
2024-07-13 17:23:45 +0530

    removing unnecessary files

--- Tulsi_King/tulsi-king/src/App.js
+++ Tulsi_King/tulsi-king/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import Header from './components/Header'
 function App() {
-  return (
+  <>
     <Header />
-  );
+    <main>
+        ABC
+    </main> 
+  </> 
 }
 
 export default App;
