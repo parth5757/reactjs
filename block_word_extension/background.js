6f5daa1e49daa7f624ab91cd08c2c745d2e6306a
parth5757 <psthakkar2@gmail.com>
2024-09-18 12:06:07 +0530

    block word extension bing target page removed

--- block_word_extension/background.js
+++ block_word_extension/background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "minitv", "mx player", "south movie", "korean","study glows", "ankit awasthi", "#status", "reels", "shorts", "Sony Sab", "affairs", "drama", "food Vlog", "street food", "Jio Cinema", "Cricket", "Pahul Walia", "rj raunac", "tanay", "Dhruv Rathee", "swagger sharma", "web series", "movie", "studyiq", "yrkkh", "bollywood", "roast", "hindi knowledge show", "pakistan", "china", "Physics Wallah", "esral", "maddam sir", "russia", "song", "sonyliv", "study i" ,"tenali", "son", "virat kohli", "Rohit Sharma", "Rohit", "IPL", "RCB", "GT", "Darshan Raval", "Arjit singh", "T seies", "neha kakkar", "Ashish Chanchalni", "Triggered inssan", "Fukra inssan", "world affairs", "unacadmy", "csk", "t-series", "tmk", "study   q", "पंडित रामा कृष्णा", "comedian", "vlogger", "travel", "comedian", "तारक मेहता का उल्टा चश्मा"]});
+  chrome.storage.local.set({ keywords: ["study iq", "tmkoc", "minitv", "mx player", "south movie", "korean","study glows", "ankit awasthi", "#status", "reels", "shorts", "Sony Sab", "affairs", "drama", "food Vlog", "street food", "Jio Cinema", "Cricket", "Pahul Walia", "rj raunac", "tanay", "Dhruv Rathee", "swagger sharma", "web series", "movie", "studyiq", "yrkkh", "bollywood", "roast", "hindi knowledge show", "pakistan", "china", "Physics Wallah", "esral", "maddam sir", "russia", "song", "sonyliv", "study i" ,"tenali", "virat kohli", "Rohit Sharma", "Rohit", "TATA IPL", "RCB", "GT vs", "Darshan Raval", "Arjit singh", "T seies", "neha kakkar", "Ashish Chanchalni", "Triggered inssan", "Fukra inssan", "world affairs", "unacadmy", "csk", "t-series", "tmk", "study   q", "पंडित रामा कृष्णा", "comedian", "vlogger", "travel", "comedian", "तारक मेहता का उल्टा चश्मा"]});
 });
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
@@ -22,3 +22,53 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     });
   }
 });
+
+
+
+// if (window.location.href.includes("https://www.linkedin.com/feed/")) {
+//   window.location.href = "https://www.linkedin.com/jobs/";
+// }
+// if (window.location.href.includes("https://www.instagram.com/reels/")) {
+//   window.location.href = "https://www.instagram.com";
+// }
+// if (window.location.href.includes("https://www.instagram.com/explore/")) {
+//   window.location.href = "https://www.instagram.com";
+// }
+// if (window.location.href.includes("https://www.youtube.com/shorts/")) {
+//   window.location.href = "https://www.youtube.com";
+// }
+
+// function checkPageContent() {
+//   const pageContent = document.documentElement.innerText; // Get the entire page content
+//   console.log(pageContent);
+//   chrome.runtime.sendMessage({ type: 'checkPageContent', content: pageContent });
+// }
+
+// let intervalId = setInterval(checkPageContent, 1000); // Check page content every second
+
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.local.set({
+//     keywords: ["study iq", "tmkoc", "minitv", "mx player", "south movie", "korean", "study glows", "ankit awasthi", "#status", "reels", "shorts", "Sony Sab", "affairs", "drama", "food Vlog", "street food", "Jio Cinema", "Cricket", "Pahul Walia", "rj raunac", "tanay", "Dhruv Rathee", "swagger sharma", "web series", "movie", "studyiq", "yrkkh", "bollywood", "roast", "hindi knowledge show", "pakistan", "china", "Physics Wallah", "esral", "maddam sir", "russia", "song", "sonyliv", "study i", "tenali", "son", "virat kohli", "Rohit Sharma", "Rohit", "IPL", "RCB", "GT", "Darshan Raval", "Arjit singh", "T seies", "neha kakkar", "Ashish Chanchalni", "Triggered inssan", "Fukra inssan", "world affairs", "unacadmy", "csk", "t-series", "tmk", "study   q", "पंडित रामा कृष्णा", "comedian", "vlogger", "travel", "comedian", "तारक मेहता का उल्टा चश्मा"]
+//   });
+// });
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === 'checkPageContent') {
+//     chrome.storage.local.get("keywords", (data) => {
+//       let keywords = data.keywords || [];
+//       for (let keyword of keywords) {
+//         if (message.content.toLowerCase().includes(keyword.toLowerCase())) {
+//           chrome.tabs.remove(sender.tab.id, () => {
+//             chrome.notifications.create({
+//               type: 'basic',
+//               iconUrl: 'icon.png',
+//               title: 'Tab Closed',
+//               message: `Closed tab because it contained the keyword: "${keyword}".`
+//             });
+//           });
+//           break;
+//         }
+//       }
+//     });
+//   }
+// });
--- block_word_extension/content.js
+++ block_word_extension/content.js
@@ -1,4 +1,6 @@
+// Remove Instagram reels and YouTube shorts pages
 if (window.location.href.includes("https://www.linkedin.com/feed/")) {
+  alert("You're on a LinkedIn feed page transfer you to LinkedIn jobs page");
   window.location.href = "https://www.linkedin.com/jobs/";
 }
 if (window.location.href.includes("https://www.instagram.com/reels/")) {
@@ -10,13 +12,22 @@ if (window.location.href.includes("https://www.instagram.com/explore/")) {
 if (window.location.href.includes("https://www.youtube.com/shorts/")) {
   window.location.href = "https://www.youtube.com";
 }
-// else if (window.location.href.includes("https://www.udemy.com/course/react-the-complete-guide-incl-redux/")) {
-//   alert("executing")
-//   $(document).ready(function() {
-//     alert("entering in the word")
-//     $('.ud-text-xl video-viewer--title-overlay--YZQuH').html('');
-//   });
-// }
+if (window.location.href.includes("https://www.bing.com/search?q=")) {
+
+  // Wait for the DOM to be fully loaded
+  window.onload = function() {
+    // Select all anchor tags on the page
+    const anchorTags = document.querySelectorAll('a');
+    
+    // Log the anchor tags to see if they are selected properly
+
+    // Loop through each anchor tag and remove the target attribute
+    anchorTags.forEach(anchor => {
+      anchor.removeAttribute('target');
+    });
+  };
+}
+
 
 function checkTitle() {
     const title = document.title;
