7c9808efc4537042d8b867e1092bcbe6c95e1ac0
parth5757 <psthakkar2@gmail.com>
2024-10-05 07:38:29 +0530

    Tulsi king header work updated

--- Tulsi_King/tulsiking/src/App.js
+++ Tulsi_King/tulsiking/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import Header from './components/Header'
 function App() {
-  <>
-    <Header />
-    <main>
-        ABC
-    </main> 
-  </> 
+    return (
+        <>
+        <Header />
+        </>
+    )
 }
 
 export default App;
